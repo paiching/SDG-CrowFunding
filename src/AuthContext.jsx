307824ea2537919254867a0efc3525f0a615b0eb
HenryLai <engine.stack@gmail.com>
2024-03-03 01:43:34 +0800

    try auth

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,7 +1,7 @@
 // AuthContext.js
 import React, { createContext, useState, useContext } from 'react';
 
-const AuthContext = createContext(null);
+const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [userInfo, setUserInfo] = useState(null);
@@ -13,4 +13,10 @@ export const AuthProvider = ({ children }) => {
   );
 };
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+      throw new Error('useAuth must be used within an AuthProvider');
+    }
+    return context;
+  };
