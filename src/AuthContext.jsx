c7e15f13b3c02ce8d36fe0086289ab67a256043e
HenryLai <engine.stack@gmail.com>
2024-03-04 00:12:44 +0800

    auth

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -5,12 +5,13 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [userInfo, setUserInfo] = useState(null);
-//   const [smartAccount, setSmartAccount] = useState(null);
+  const [smartAccount, setSmartAccount] = useState(null);
   const [CAaddress, setCAaddress] = useState(null); 
+
   //這兩個參數由signin設置過來
 
   return (
-    <AuthContext.Provider value={{ userInfo, setUserInfo, CAaddress, setCAaddress }}>
+    <AuthContext.Provider value={{ userInfo, setUserInfo, CAaddress, setCAaddress,smartAccount, setSmartAccount}}>
       {children}
     </AuthContext.Provider>
   );
@@ -22,12 +23,4 @@ export const useAuth = () => {
       throw new Error('useAuth must be used within an AuthProvider');
     }
     return context;
-  };
-// 新的钩子用于访问和设置 smartAccount
-export const useSmartAccount = () => {
-    const context = useContext(AuthContext);
-    if (!context) {
-      throw new Error('useSmartAccount must be used within an AuthProvider');
-    }
-    return [context.smartAccount, context.setSmartAccount];
   };
